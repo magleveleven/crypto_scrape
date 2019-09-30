48a140b10bcf78185c15724098067c70bacc588b
Eric Ballesteros <ballesteros@icloud.com>
2019-09-30 16:05:17 -0500

    schemas updated

---
+++ models/article.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "note"
+    }
+});
+
+var article = mongoose.model("article", ArticleSchema);
+
+module.exports = article
---
+++ models/note.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var NoteSchema = new Schema ({
+
+    title: String,
+    body: String
+});
+
+var note = mongoose.model("note", NoteSchema);
+
+module.exports = note;
